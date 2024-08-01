563ef2051c1633aecbcdd11467e4a497a064457e
Lucasch1 <lucasch2011@gmail.com>
2024-08-01 19:18:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+async function getPlaces() {
+    const response = await client.textSearch({
+        params: {
+            query: "Tiendas 3B", // Substitua pelo nome desejado
+            location: "19.435332, -99.130427", // Mexico City (latitude, longitude)
+            radius: 1000000, // Raio de busca em metros
+            key: apiKey, // Substitua pela sua chave de API
+            type: "supermarket", // Tipo de lugar a ser buscado
+        },
+    });
+
+    if (response.data.status === "OK") {
+        const places = response.data.results;
+        // Faça o que quiser com os resultados (places)
+        console.log(places);
+        return places;
+    } else {
+        console.error("Erro na busca:", response.data.status);
+    }
+}
+
+getPlaces();
